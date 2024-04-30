79dc089775457035e08ef03da6f21073bf7414a8
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-04-29 20:27:07 -0400

    add print for pack function

--- functions_practice.py
+++ functions_practice.py
@@ -5,9 +5,11 @@ def pack(a, b, c):
     return [a, b, c]
 
 def eat_lunch(foodItems):
+    # check if no food passed in
     if len(foodItems) == 0:
         print("My lunchbox is empty!")
     else:
+        # print out each item passed in
         for i in range(len(foodItems)):
             if i == 0:
                 print(f"First I eat {foodItems[0]}")
@@ -15,5 +17,5 @@ def eat_lunch(foodItems):
                 print(f"Next I eat {foodItems[i]}")
 
 hello()
-pack(1,3,6)
+print(pack(1,3,6))
 eat_lunch(["apple", "banana", "carrot"])
