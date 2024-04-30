b306e81121a7dce98ab13ea347303b16cebc101e
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-04-29 20:23:44 -0400

    functions practice completed

--- functions_practice.py
+++ functions_practice.py
@@ -4,4 +4,16 @@ def hello():
 def pack(a, b, c):
     return [a, b, c]
 
-def eat_lunch():
+def eat_lunch(foodItems):
+    if len(foodItems) == 0:
+        print("My lunchbox is empty!")
+    else:
+        for i in range(len(foodItems)):
+            if i == 0:
+                print(f"First I eat {foodItems[0]}")
+            else:
+                print(f"Next I eat {foodItems[i]}")
+
+hello()
+pack(1,3,6)
+eat_lunch(["apple", "banana", "carrot"])
